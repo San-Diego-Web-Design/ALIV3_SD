fd82abb251580a92b39ed8c6b6fcdb5ed510f969
Andrew Wilhelm <AndrewWilhelmEX@gmail.com>
2011-06-08 01:18:37 -0700

    doo doo baby

--- sites/all/themes/aliv3/js/custom.js
+++ sites/all/themes/aliv3/js/custom.js
@@ -3,7 +3,42 @@
     attach: function(context, settings) {
       // you can implement your custom javascript/jquery here,
       // and also create other attached behaviors
-      
+      $(document).ready(function(){
+
+	    $("ul#superfish-3 li.last a").click(function(){
+	        $("div#block-user-login").slideToggle("slow");
+	        $(this).toggleClass("sign-in-active"); 
+			return false;
+	    });
+
+
+	});
     }
   };
 })(jQuery);
+
+
+
+/*
+$(document).ready(function(){
+	$("#user-login-form #edit-name-wrapper input").focus(function() {
+		$("#user-login-form #edit-name-wrapper label").hide("fast");	
+	});
+	$("#user-login-form #edit-name-wrapper input").blur(function() {
+		if ($(this).val() === "") {
+			$("#user-login-form #edit-name-wrapper label").show("fast");
+		}
+	});
+});
+
+$(document).ready(function(){
+	$("#user-login-form #edit-pass-wrapper input").focus(function() {
+		$("#user-login-form #edit-pass-wrapper label").hide("fast");	
+	});
+	$("#user-login-form #edit-pass-wrapper input").blur(function() {
+		if ($(this).val() === ""){
+			$("#user-login-form #edit-pass-wrapper label").show("fast");
+		}
+	});
+});
+*/
